0a3d67afe735ddb67d90dc8b9cceafcf537b9374
Pavan Kalyan <komarina.pavankalyan@gmail.com>
2021-09-01 22:20:05 +0530

    Add mysql db

--- concurrency/concurrency/settings.py
+++ concurrency/concurrency/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'concurrency.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_db', # change here
+        'USER': 'testuser', # change user
+        'PASSWORD': 'Test@123', # change password
+        'HOST': '',
+        'PORT': '3306',
     }
 }
 
