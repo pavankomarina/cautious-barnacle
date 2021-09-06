5e9449fd3c640ade9fa7a72cd5f7d04b9037b094
Pavan Kalyan <komarina.pavankalyan@gmail.com>
2021-09-06 22:14:40 +0530

    Update example 4

--- threading-examples/example_4.py
+++ threading-examples/example_4.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
+""" Threads that waste CPU cycles """
+
 import os
+import threading
+import multiprocessing as mp
+
+# a simple function that wastes CPU cycles forever
+def cpu_waster():
+    while True:
+        pass
+
+
