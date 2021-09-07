f0ada69cc0fb315ff92d66a69571deb4616cb9ed
Pavan Kalyan <komarina.pavankalyan@gmail.com>
2021-09-07 22:20:07 +0530

    Update example 3

--- threading-examples/example_3.py
+++ threading-examples/example_3.py
@@ -9,3 +9,24 @@ total_count = 0
 book_count = 0
 pencil_count = 0
 
+class ThreadLockDemo:
+    lock = threading.RLock()
+
+    def count_books(self):
+        global total_count
+        global book_count
+        for _ in range(10_000_000):
+            self.lock.acquire() # lock the common object
+            total_count += 2
+            self.lock.release()
+            book_count += 1 # only this method uses book_count so no lock required.
+
+    def count_pencils(self):
+        global total_count
+        global pencil_count
+        for _ in range(20_000_000):
+            self.lock.acquire() # lock the common object
+            total_count += 5
+            self.lock.release()
+            pencil_count +=1  # only this method uses pencil_count so no lock required.
+
