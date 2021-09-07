780c994c197b697af4261d436ebf44606793067e
Pavan Kalyan <komarina.pavankalyan@gmail.com>
2021-09-07 22:19:50 +0530

    Update example 3

--- threading-examples/example_3.py
+++ threading-examples/example_3.py
@@ -1,11 +1,11 @@
-# MAX_Threads
-#wait() method
-"""
-Lock objects
-Rlock
-acquire()
-release()
-wait()
+# Problem statement:
+# When two or more threads accessing the same resources , data race might happen
+# Locking is used to resolve these type of issues
+# Following example demonstrates the locking mechanism in Python.
 
-daemon thread
-"""
+import threading
+
+total_count = 0
+book_count = 0
+pencil_count = 0
+
